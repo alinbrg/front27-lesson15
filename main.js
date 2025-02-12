f84cfda5ebe8e828f933fb4cfe38eb1076049d6b
Alina Bregvadze <alina.bregvadze@gmail.com>
2025-02-12 21:51:30 +0400

    lesson 15

---
+++ main.js
@@ -0,0 +1,98 @@
+/* 
+console.log("Hello World");
+
+*/
+
+// console.log("Hello World");
+
+// consollog("Hello World");
+
+console.log("Hello World");
+
+// userAge = 30;
+// var userName = "John Doe";
+
+// console.log(userName);
+
+let userEmail = "example@gmail.com";
+const userCountry = "India";
+
+userEmail = "example@edu.gov.ge";
+
+// userCountry = "USA";
+// console.log(userEmail);
+
+const userLastName = "Doe";
+const userAge = 30;
+const isUserEmployed = true;
+let userSalary = undefined;
+let userAddress = null;
+
+//
+
+// userSalary = 1000;
+
+// console.log(userLastName, typeof userLastName);
+// console.log(userAge, typeof userAge);
+// console.log(isUserEmployed, typeof isUserEmployed);
+// console.log(userSalary, typeof userSalary);
+// console.log(userAddress, typeof userAddress);
+
+const product1 = 111;
+const product2 = 200;
+const discount = 50;
+
+// multiplication
+const quantity = 3;
+const product1TotalPrice = product1 * quantity;
+console.log(product1TotalPrice);
+
+// addition
+const totalPrice = product1TotalPrice + product2;
+console.log(totalPrice);
+
+// subtraction
+const discountedPrice = totalPrice - discount;
+console.log(discountedPrice);
+
+// division
+const pricePerItem = totalPrice / 4;
+console.log(pricePerItem);
+
+// remainder ნაშთი
+const remainder = totalPrice % 100;
+
+console.log("remainder", remainder);
+
+// power
+const power = 2 ** 3;
+// 2*2*2
+console.log(power);
+
+// console.log(typeof NaN)
+// NaN
+// console.log(10 / 0);
+// Infinity
+// console.log(-10 / 0);
+// -Infinity
+
+const product3 = "test";
+const sum = totalPrice + 10 + product3;
+console.log(sum);
+
+const message = "Hello" + " " + "your total price is " + totalPrice;
+
+console.log(message);
+
+const message2 = "test tex\"t's information ${totalPrice}";
+const message3 = "test text\"s   'information ${totalPrice}";
+console.log(message2);
+console.log(message3);
+const message4 = `test text's  
+
+
+"information"`;
+console.log(message4);
+
+const message5 = `Hello your total price is ${totalPrice}`;
+console.log(message5);
